73807b5d6720552b823f4dd4a8f08bb2c0db2174
londonFlynn <47341087+londonFlynn@users.noreply.github.com>
2020-07-23 09:43:07 -0600

    DiceRolling

---
+++ DiceRollerClicker/StoreItemCosts.cs
@@ -0,0 +1,13 @@
+﻿namespace DiceRollerClicker
+{
+    enum StoreItemCosts
+    {
+        d6 = 0,
+        d8 = 0,
+        d10 = 0,
+        d12 = 0,
+        d20 = 0,
+        additionalDie = 0,
+        goblin = 0
+    }
+}
