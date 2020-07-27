b454dc3a1d3ec09d59798aad33d31f0ccc205c21
londonFlynn <47341087+londonFlynn@users.noreply.github.com>
2020-07-27 08:40:05 -0600

    GameManager

--- DiceRollerClicker/DiceBag.cs
+++ DiceRollerClicker/DiceBag.cs
@@ -16,7 +16,6 @@ namespace DiceRollerClicker
         }
         public void UpgradeDie(int sidesToUpgradeFrom, int sidesToUpgradeTo)
         {
-
             if (Dice[sidesToUpgradeFrom] > 0)
             {
                 Dice[sidesToUpgradeFrom] -= 1;
@@ -35,12 +34,12 @@ namespace DiceRollerClicker
                 var count = Dice[key];
                 if (count > 0)
                 {
-                    total += fibanachiRoll(0, count, new Die { Sides = key });
+                    total += halvingRoll(0, count, new Die { Sides = key });
                 }
             }
             return total;
         }
-        private long fibanachiRoll(long runningTotal, int CountOfDie, Die die)
+        private long halvingRoll(long runningTotal, int CountOfDie, Die die)
         {
             if (CountOfDie == 1)
             {
@@ -48,23 +47,37 @@ namespace DiceRollerClicker
             }
             else
             {
-                int fib = LargestFibanachiValueLessThanCount(CountOfDie);
-                CountOfDie -= fib;
-                runningTotal += (die.Roll() * fib) + fibanachiRoll(runningTotal, CountOfDie, die);
+                int half = CountOfDie / 2;
+                CountOfDie = CountOfDie - half;
+                runningTotal += (die.Roll() * half) + halvingRoll(runningTotal, CountOfDie, die);
             }
             return runningTotal;
         }
-        private int LargestFibanachiValueLessThanCount(int count)
-        {
-            List<int> fibbanachiValues = new List<int>();
-            fibbanachiValues.Add(1);
-            fibbanachiValues.Add(1);
-            while (fibbanachiValues[fibbanachiValues.Count - 1] < count)
-            {
-                fibbanachiValues.Add(fibbanachiValues[fibbanachiValues.Count - 1] + fibbanachiValues[fibbanachiValues.Count - 2]);
-                fibbanachiValues.RemoveAt(0);
-            }
-            return fibbanachiValues[fibbanachiValues.Count - 2];
-        }
+        //private long fibanachiRoll(long runningTotal, int CountOfDie, Die die)
+        //{
+        //    if (CountOfDie == 1)
+        //    {
+        //        runningTotal += die.Roll();
+        //    }
+        //    else
+        //    {
+        //        int fib = LargestFibanachiValueLessThanCount(CountOfDie);
+        //        CountOfDie -= fib;
+        //        runningTotal += (die.Roll() * fib) + fibanachiRoll(runningTotal, CountOfDie, die);
+        //    }
+        //    return runningTotal;
+        //}
+        //private int LargestFibanachiValueLessThanCount(int count)
+        //{
+        //    List<int> fibbanachiValues = new List<int>();
+        //    fibbanachiValues.Add(1);
+        //    fibbanachiValues.Add(1);
+        //    while (fibbanachiValues[fibbanachiValues.Count - 1] < count)
+        //    {
+        //        fibbanachiValues.Add(fibbanachiValues[fibbanachiValues.Count - 1] + fibbanachiValues[fibbanachiValues.Count - 2]);
+        //        fibbanachiValues.RemoveAt(0);
+        //    }
+        //    return fibbanachiValues[fibbanachiValues.Count - 2];
+        //}
     }
 }
