cced83e93fd5d03a046ff7094a0f63fb943d4409
ElijahHarris <47341441+ElijahHarris@users.noreply.github.com>
2020-07-28 09:23:25 -0600

    Add .cs files back

---
+++ DiceRollerClicker/DiceBag.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+
+namespace DiceRollerClicker
+{
+    class DiceBag
+    {
+        public Dictionary<int, int> Dice = new Dictionary<int, int>();
+        public DiceBag()
+        {
+            Dice.Add(4, 0);
+            Dice.Add(6, 0);
+            Dice.Add(8, 0);
+            Dice.Add(10, 0);
+            Dice.Add(12, 0);
+            Dice.Add(20, 0);
+        }
+        public void UpgradeDie(int sidesToUpgradeFrom, int sidesToUpgradeTo)
+        {
+            if (Dice[sidesToUpgradeFrom] > 0)
+            {
+                Dice[sidesToUpgradeFrom] -= 1;
+                Dice[sidesToUpgradeTo] += 1;
+            }
+            else
+            {
+                throw new System.Exception("No die of that size to upgrade");
+            }
+        }
+        public long Roll()
+        {
+            long total = 0;
+            foreach (var key in Dice.Keys)
+            {
+                var count = Dice[key];
+                if (count > 0)
+                {
+                    total += halvingRoll(0, count, new Die { Sides = key });
+                }
+            }
+            return total;
+        }
+        private long halvingRoll(long runningTotal, int CountOfDie, Die die)
+        {
+            if (CountOfDie == 1)
+            {
+                runningTotal += die.Roll();
+            }
+            else
+            {
+                int half = CountOfDie / 2;
+                CountOfDie = CountOfDie - half;
+                runningTotal += (die.Roll() * half) + halvingRoll(runningTotal, CountOfDie, die);
+            }
+            return runningTotal;
+        }
+        //private long fibanachiRoll(long runningTotal, int CountOfDie, Die die)
+        //{
+        //    if (CountOfDie == 1)
+        //    {
+        //        runningTotal += die.Roll();
+        //    }
+        //    else
+        //    {
+        //        int fib = LargestFibanachiValueLessThanCount(CountOfDie);
+        //        CountOfDie -= fib;
+        //        runningTotal += (die.Roll() * fib) + fibanachiRoll(runningTotal, CountOfDie, die);
+        //    }
+        //    return runningTotal;
+        //}
+        //private int LargestFibanachiValueLessThanCount(int count)
+        //{
+        //    List<int> fibbanachiValues = new List<int>();
+        //    fibbanachiValues.Add(1);
+        //    fibbanachiValues.Add(1);
+        //    while (fibbanachiValues[fibbanachiValues.Count - 1] < count)
+        //    {
+        //        fibbanachiValues.Add(fibbanachiValues[fibbanachiValues.Count - 1] + fibbanachiValues[fibbanachiValues.Count - 2]);
+        //        fibbanachiValues.RemoveAt(0);
+        //    }
+        //    return fibbanachiValues[fibbanachiValues.Count - 2];
+        //}
+    }
+}
---
+++ DiceRollerClicker/GameManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DiceRollerClicker
+{
+    class GameManager
+    {
+        public DiceBag DiceBag = new DiceBag();
+        public int GoblinCount = 0;
+        public long Score = 0;
+        public readonly Dictionary<string, int> StoreItemCosts = new Dictionary<string, int>();
+        const int DefaultDieCost = 0;
+
+        public GameManager()
+        {
+            StoreItemCosts.Add("Goblin", 0);
+            StoreItemCosts.Add("NewD4", 5 * DefaultDieCost);
+            StoreItemCosts.Add("NewD6", 7 * DefaultDieCost);
+            StoreItemCosts.Add("NewD8", 9 * DefaultDieCost);
+            StoreItemCosts.Add("NewD10", 11 * DefaultDieCost);
+            StoreItemCosts.Add("NewD12", 13 * DefaultDieCost);
+            StoreItemCosts.Add("NewD20", 21 * DefaultDieCost);
+            DiceBag.Dice.Add(4, 1);
+            doGoblinClicks();
+        }
+        private async void doGoblinClicks()
+        {
+            do
+            {
+                int GoblinsYetToClick = GoblinCount;
+                for (int i = 0; i < 50; i++)
+                {
+                    if (GoblinsYetToClick > 0)
+                    {
+                        int GoblinsToClickThisCheck = GoblinCount / 50;
+                        GoblinsToClickThisCheck = GoblinsToClickThisCheck <= GoblinsYetToClick ? GoblinsToClickThisCheck : GoblinsYetToClick;
+                        for (int j = 0; j < GoblinsToClickThisCheck; j++)
+                        {
+                            Click();
+                        }
+                        GoblinsYetToClick -= GoblinsToClickThisCheck;
+                    }
+                    await Task.Delay(20);
+                }
+            } while (true);
+        }
+
+        public void Click()
+        {
+            Score += DiceBag.Roll();
+        }
+        public void BuyGoblin()
+        {
+            Score -= StoreItemCosts["Goblin"];
+            GoblinCount++;
+        }
+        public void BuyD4()
+        {
+            Score -= StoreItemCosts["NewD4"];
+            DiceBag.Dice[4] = DiceBag.Dice[4] + 1;
+        }
+        public void BuyD6()
+        {
+            Score -= StoreItemCosts["NewD6"];
+            DiceBag.Dice[6] = DiceBag.Dice[6] + 1;
+        }
+        public void BuyD8()
+        {
+            Score -= StoreItemCosts["NewD8"];
+            DiceBag.Dice[8] = DiceBag.Dice[8] + 1;
+        }
+        public void BuyD10()
+        {
+            Score -= StoreItemCosts["NewD10"];
+            DiceBag.Dice[10] = DiceBag.Dice[10] + 1;
+        }
+        public void BuyD12()
+        {
+            Score -= StoreItemCosts["NewD12"];
+            DiceBag.Dice[12] = DiceBag.Dice[12] + 1;
+        }
+        public void BuyD20()
+        {
+            Score -= StoreItemCosts["NewD20"];
+            DiceBag.Dice[20] = DiceBag.Dice[20] + 1;
+        }
+    }
+}
